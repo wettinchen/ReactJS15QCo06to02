c97d7f2381a9bf0c7edd1e9ae0f9012dc59c20dc
suncrownfisher <suncrownfisher@gmail.com>
2023-04-16 01:21:35 +0800

    first commit

---
+++ src/Form.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Form = ({ requestType, setRequestType }) => {
+  return (
+    <form onSubmit={(event) => event.preventDefault()}>
+        
+    </form>
+  )
+}
+
+export default Form
